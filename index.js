ab831a8c8fc1cf7d0005fbff5d65376b931eac6e
Alexander Obregon <alexander.obregon47@gmail.com>
2021-12-21 13:05:55 -0800

    commit tree 3738689c12c5c361086ae6de609fafc1dd86fcfd

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(data => renderBooks(data));
+  }
   
-}
 
-function renderBooks(books) {
+function renderBooks(book) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  book.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
@@ -13,5 +15,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+ fetchBooks();
+
+})
